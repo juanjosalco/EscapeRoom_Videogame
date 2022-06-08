8fbd594368e42328e4dbe90c633ab70f66f46808
carloseag1234 <carloseag1234@gmail.com>
2022-06-08 12:01:28 -0500

    Divide .h and .cpp of Item class

--- Item.h
+++ Item.h
@@ -18,34 +18,5 @@ class Item {
         std::string name;
 };
 
-Item::Item(){
-    description="Este es una cosita bonita";
-    name="";
-}
-
-Item::Item(std::string name_, std::string des){
-    description=des;
-    name=name_;
-}
-
-std::string Item::getName()const{
-    return name;
-}
-
-std::string Item::getDescription() const{
-    return description;
-}
-
-void Item::setName(std::string name_){
-    name=name_;
-}
-
-void Item::setDescription(std::string descp){
-    description=descp;
-}
-
-std::string Item::getDescripcionLarga(){
-    return name+ ": \n" + description ;
-}
 
 #endif //ITEM_H
